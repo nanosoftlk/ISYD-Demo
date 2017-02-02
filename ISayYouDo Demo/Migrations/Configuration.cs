8e62c42e6bb908cdf220e56f77c332e617ff0c0f
nanosoftlk <sanjeewa@nanosoft.lk>
2017-02-02 22:47:26 +0530

    Enabled code first migration.

---
+++ ISayYouDo Demo/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace ISayYouDo_Demo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ISayYouDo_Demo.DbModel>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "ISayYouDo_Demo.DbModel";
+        }
+
+        protected override void Seed(ISayYouDo_Demo.DbModel context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
